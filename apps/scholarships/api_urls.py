d5c713ba3e3f7dc4192c21e7211df65507c25516
Roy Okola <royokola3@gmail.com>
2026-08-12 15:02:12 +0300

    Cleanup legacy Django AI features in favor of Next.js native implementation

--- apps/scholarships/api_urls.py
+++ apps/scholarships/api_urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from .api_views import CountryViewSet, FieldViewSet, ScholarshipViewSet, SearchView, CSVUploadView
+from .api_views import CountryViewSet, FieldViewSet, ScholarshipViewSet, SearchView
 
 router = DefaultRouter()
 router.register('scholarships', ScholarshipViewSet, basename='scholarship')
@@ -11,5 +11,4 @@ router.register('fields', FieldViewSet, basename='field')
 app_name = 'api'
 urlpatterns = [
     path('search/', SearchView.as_view(), name='search'),
-    path('scholarships/upload_csv/', CSVUploadView.as_view(), name='upload_csv'),
 ] + router.urls
